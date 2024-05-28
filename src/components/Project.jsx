18f9ad8322b6c50a9e930e32c034e9ef4e9dda52
yashG0 <yashgaurkar0@gmail.com>
2024-05-28 08:10:01 +0530

    project section fixed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -23,7 +23,7 @@ const Project = () => {
 
     fetch('https://portfolio-backend-2-production.up.railway.app/api/project/')
       .then(response => response.json())
-      .then(data => setProjectData(data.data))
+      .then(data => setProjectData(data))
       .catch(error => console.error('Error fetching data:', error));
   }, []);
   console.log(projectdata);
@@ -36,17 +36,17 @@ const Project = () => {
         {projectdata ? (
           projectdata.map(project => (
             <>
-              <div key={project.project_name} className="hidden md:block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-xl">
+              <div key={project._id} className="hidden md:block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-xl">
                 <div>
-                  <img className="rounded-t-lg" src={project.project_img_url} alt="project image" />
+                  <img className="rounded-t-lg" src={project.imageUrl} alt="project image" />
                 </div>
 
                 <div className="p-5">
                   <div>
-                    <h5 className="cursor-pointer mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.project_name}</h5>
+                    <h5 className="cursor-pointer mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.name}</h5>
                   </div>
-                  <p className="cursor-pointer mb-3 font-normal text-gray-700 dark:text-gray-400 h-[139px] overflow-y-hidden">{project.project_description}</p>
-                  <a href={project.project_source_code} target='_blank' className="inline-flex items-center font-medium text-center text-white ">
+                  <p className="cursor-pointer mb-3 font-normal text-gray-700 dark:text-gray-400 h-[139px] overflow-y-hidden">{project.description}</p>
+                  <a href={project.sourceCodeUrl} target='_blank' className="inline-flex items-center font-medium text-center text-white ">
                     <Button>
                       Read more
                       <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
