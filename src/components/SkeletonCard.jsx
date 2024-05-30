963b346a401f27f3c412771371431715db65997e
yashG0 <yashgaurkar0@gmail.com>
2024-05-30 09:57:23 +0530

    skleton chan

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const SkeletonCard = () => {
   return (
-    <div className="block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-xl p-5">
+    <div className="block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-xl">
       <Skeleton variant="rectangular" className="h-[139px] w-full rounded-t-lg" />
       <div className="space-y-2 mt-5">
         <Skeleton variant="text" className="h-6 w-[80%]" />
